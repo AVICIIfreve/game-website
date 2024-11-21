da66b589eb676c41152cea7b29eb7c4dc6e567b3
wzy <759799349@qq.com>
2024-11-21 17:02:19 +0800

    Displaying platform icons

--- src/components/GameGrid.tsx
+++ src/components/GameGrid.tsx
@@ -3,10 +3,18 @@ import apiClient from "../service/api-client";
 import { SimpleGrid } from "@chakra-ui/react";
 import GameCard from "./GameCard";
 
+export interface Platform {
+  id: number;
+  name: string;
+  slug: string;
+  image_background: string;
+}
+
 export interface Game {
   id: number;
   name: string;
   background_image: string;
+  parent_platforms: { platform: Platform }[];
 }
 
 interface FetchGamesResponse {
@@ -23,7 +31,7 @@ const GameGrid = () => {
       .get<FetchGamesResponse>("/games")
       .then((res) => setGames(res.data.results))
       .catch((err) => setError(err.message));
-  });
+  }, []);
 
   return (
     <>
--- src/service/api-client.ts
+++ src/service/api-client.ts
@@ -4,6 +4,6 @@ import axios from "axios";
 export default axios.create({
     baseURL: "https://api.rawg.io/api",
     params: {
-        key: "6fbc1eca65e3458bb1ae905bb45cff9b"
+        key: "2d2ba0f52d1f4d658fcaaf560d6c2a5e"
     }
 })
